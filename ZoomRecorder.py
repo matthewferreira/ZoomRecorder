68068c02d5c92c33f7d5cf493b4f722d4330fe2c
Matthew <mferreira2@student.sierracollege.edu>
2020-04-16 12:06:49 -0700

    Changes: changed login function to return browser, made the flow of the main program

--- ZoomRecorder.py
+++ ZoomRecorder.py
@@ -9,13 +9,20 @@ import os
 import pprint as pp
 from Course import Course
 import subprocess
+import platform
+
+desktop = 'DESKTOP-6BL58T5'
+laptop = 'LAPTOP-K5N3I4ET'
+
+#  detecting if program is being run on my laptop or desktop since my User names are different
+if platform.uname().node == desktop:
+    current_user = 'Matthew'
+elif plaform.uname().node == laptop:
+    current_user = 'Matthew Ferreira'
 
 
 #  creating a browser
-browser = webdriver.Firefox(executable_path=r'C:\Users\Matthew\Downloads\geckodriver.exe')
-browser.set_window_position(0, 0)
-browser.set_window_size(1920, 1080)
-browser.get('https://csus.instructure.com/')
+browser = ''
 
 #  making keyboard and mouse controllers to navigate the PC automatically
 keyboard = kb.Controller()
@@ -33,6 +40,12 @@ password = os.environ.get('CSUS_PASSWORD')
 
     #  METHOD - logging in to csus canvas
 def login():
+
+    browser = webdriver.Firefox(executable_path='C:\\Users\\' + current_user + '\\Downloads\\geckodriver.exe')
+    browser.set_window_position(0, 0)
+    browser.set_window_size(1920, 1080)
+    browser.get('https://csus.instructure.com/')
+
     #  entering username into username text field
     user_elem = browser.find_element_by_css_selector('#username')
     user_elem.send_keys(username)
@@ -44,10 +57,11 @@ def login():
 
     #  giving time for browser to load.
     time.sleep(5)
+    return browser
 
 
     #  METHOD - getting newest meeting id from canvas course
-def get_meeting_id(course: Course):
+def get_meeting_id(browser, course: Course):
     #  courseNum is a variable for the number used in the HTML on canvas. for example: my csc 131 class is n-th child(1)
     if course.courseName == 'csc137':
         course.courseNum = 2
@@ -79,7 +93,7 @@ def get_meeting_id(course: Course):
 def join_meeting(course: Course):
 
     #  starting a subprocess that opens up the Zoom app on my pc
-    zoom = subprocess.run(['start', r'C:\Users\Matthew\AppData\Roaming\Zoom\bin\Zoom.exe'], shell=True)
+    zoom = subprocess.run(['start', 'C:\\Users\\' + current_user + '\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe'], shell=True)
 
     time.sleep(2)
 
@@ -95,10 +109,19 @@ def join_meeting(course: Course):
     mouse.press(ms.Button.left)
     mouse.release(ms.Button.left)
 
+    time.sleep(2)
     #  typing the meeting ID we found into the text box and pressing enter
     keyboard.type(course.meeting_id)
     keyboard.press(kb.Key.enter)
     keyboard.release(kb.Key.enter)
+
+    time.sleep(2)
+    mouse.click(ms.Button.left, 1)
+    keyboard.press(kb.Key.cmd)
+    keyboard.press(kb.Key.up)
+    time.sleep(0.5)
+    keyboard.release(kb.Key.up)
+    keyboard.release(kb.Key.cmd)
 
 
 def record():
@@ -106,14 +129,15 @@ def record():
     keyboard.press(kb.Key.cmd)
     keyboard.press(kb.Key.alt)
     keyboard.press('r')
-    time.sleep(0.3)
+    time.sleep(0.5)
     keyboard.release(kb.Key.cmd)
     keyboard.release(kb.Key.alt)
     keyboard.release('r')
 
 
-
-login()
-get_meeting_id(csc137)
+get_meeting_id(login(), csc137)
 join_meeting(csc137)
+time.sleep(10)
+record()
+
 
